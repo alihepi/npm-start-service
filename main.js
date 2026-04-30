1cfdaee86daf48d82d0b5c4fbabc7e77643bf5a7
alihepi <aliimutlu@hotmail.com>
2026-04-30 14:23:22 +0300

    feat: electron tray app

---
+++ main.js
@@ -0,0 +1,41 @@
+﻿const { app, Tray, Menu, shell } = require("electron");
+const { exec, execSync } = require("child_process");
+const path = require("path");
+
+const PROJECT_ROOT = "C:\\Users\\Bim2\\alihepi\\IMS\\Inventory-Management-System";
+let tray = null;
+let npmProcess = null;
+
+function startNpm() {
+  if (npmProcess) return;
+  npmProcess = exec("npm start", { cwd: PROJECT_ROOT, windowsHide: true });
+}
+
+function stopNpm() {
+  if (!npmProcess) return;
+  try {
+    execSync(`taskkill /F /T /PID ${npmProcess.pid}`, { windowsHide: true });
+  } catch(e) {}
+  npmProcess = null;
+}
+
+app.whenReady().then(() => {
+  app.setAppUserModelId("IMS Server");
+
+  startNpm();
+
+  tray = new Tray(path.join(__dirname, "icon.png"));
+  tray.setToolTip("IMS Server");
+
+  const menu = Menu.buildFromTemplate([
+    { label: "Arayuzu Ac", click: () => shell.openExternal("http://localhost:5173") },
+    { label: "API Ac",     click: () => shell.openExternal("http://localhost:3000") },
+    { type: "separator" },
+    { label: "Yeniden Baslat", click: () => { stopNpm(); startNpm(); } },
+    { label: "Cikis", click: () => { stopNpm(); app.quit(); } },
+  ]);
+
+  tray.setContextMenu(menu);
+});
+
+app.dock?.hide();
